c0c0a39d1c062cadd1b9ef6a2dd7c4a77bc92441
Bruno Rodrigues <bytebruno@gmail.com>
2018-06-19 16:07:04 -0300

    Criação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
+import Places from './components/Places';
 
 class App extends Component {
   render() {
@@ -11,7 +13,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Map />
+          <Places />
         </p>
       </div>
     );
